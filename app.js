e0f0c2a8da340ae66d72ed85735a89fbe88706ab
shubhamgondhali01 <shubhamgondhali30@gmail.com>
2024-06-23 17:10:03 +0530

    complete project

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const { body, validationResult } = require('express-validator');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
 
 const userRoutes = require('./routes/users');
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
+const User = require('./models/user');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
+const SSL_KEY_PATH = './Keys/server.key';
+const SSL_CERT_PATH = './Keys/server.crt';
 
 // Middleware
 app.use(bodyParser.json());
@@ -31,6 +37,7 @@ const validateUserRegistration = [
   body('email').isEmail().normalizeEmail(),
   body('password').isLength({ min: 6 }),
 ];
+
 // Apply input validation middleware to specific routes
 app.post('/api/users/register', validateUserRegistration, async (req, res) => {
   const errors = validationResult(req);
@@ -40,7 +47,10 @@ app.post('/api/users/register', validateUserRegistration, async (req, res) => {
 
   // Proceed with user registration
   try {
-    // Your registration logic here
+    const { email, password, role } = req.body;
+    const newUser = new User({ email, password, role });
+    await newUser.save();
+    res.status(201).json(newUser);
   } catch (error) {
     console.error('Error registering user:', error.message);
     res.status(500).json({ message: 'Failed to register user' });
@@ -60,14 +70,21 @@ app.use((err, req, res, next) => {
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    
+    // Read SSL certificate and key
+    const sslOptions = {
+      key: fs.readFileSync(path.resolve(__dirname, SSL_KEY_PATH)),
+      cert: fs.readFileSync(path.resolve(__dirname, SSL_CERT_PATH)),
+    };
+
+    // Start HTTPS server
+    https.createServer(sslOptions, app).listen(PORT, () => {
+      console.log(`Server running on https://localhost:${PORT}`);
     });
   })
   .catch(err => console.error('MongoDB connection error:', err.message));
